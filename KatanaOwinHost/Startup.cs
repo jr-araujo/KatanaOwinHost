ed8d96a8a54e7fea56ec1f4319121ee3594c7742
José Roberto Araújo <jroberto_junior@hotmail.com>
2016-01-12 01:03:20 -0200

    Upload do projeto

---
+++ KatanaOwinHost/Startup.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+
+[assembly: OwinStartup("Desenv", typeof(KatanaOwinHost.DesenvolvimentoStartup))]
+[assembly: OwinStartup("Teste", typeof(KatanaOwinHost.TesteStartup))]
+[assembly: OwinStartup("Prod", typeof(KatanaOwinHost.Producaoptartup))]
+
+namespace KatanaOwinHost
+{
+    public class DesenvolvimentoStartup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(context =>
+            {
+                context.Response.ContentType = "text/plain";
+                context.Response.WriteAsync("");
+                return context.Response.WriteAsync("Vamos começar a desenvolver o nosso projeto? :D");
+            });
+        }
+    }
+
+    public class TesteStartup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(context =>
+            {
+                context.Response.ContentType = "text/plain";
+                context.Response.WriteAsync("");
+                return context.Response.WriteAsync("Ok, agora estou pronto para receber uma bateria de testes ");
+            });
+        }
+    }
+
+    public class Producaoptartup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseWelcomePage();
+        }
+    }
+}
